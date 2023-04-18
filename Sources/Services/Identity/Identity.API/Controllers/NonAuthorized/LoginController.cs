c28aea6b3f44fbafaed2355acd3f06a8831a520f
DanskCarvalho <danskcarvalho@gmail.com>
2023-04-18 19:32:36 -0300

    A minor adjustment

--- Sources/Services/Identity/Identity.API/Controllers/NonAuthorized/LoginController.cs
+++ Sources/Services/Identity/Identity.API/Controllers/NonAuthorized/LoginController.cs
@@ -66,17 +66,17 @@ public class LoginController : IdentityController
         async Task SignInAsync(UsuarioLogadoDTO userInfo, LoginDTO login, AuthenticationProperties props)
         {
             var claims = new List<Claim> {
-            new Claim("sub", GetSub(userInfo, login)),
-            new Claim("email", userInfo.Email ?? string.Empty),
-            new Claim("username", userInfo.NomeUsuario),
-            new Claim("uag", userInfo.IsSuperUsuario ? "true" : "false"),
-            new Claim("uad", login.EstabelecimentoId == null && userInfo.Dominios.FirstOrDefault(d => d.Id == login.DominioId)?.IsAdministrador == true ? "true" : "false"),
-            new Claim("d", login.DominioId != null && login.EstabelecimentoId == null ? login.DominioId : string.Empty),
-            new Claim("de", login.DominioId != null && login.EstabelecimentoId != null ? login.DominioId : string.Empty),
-            new Claim("e", login.EstabelecimentoId != null ? login.EstabelecimentoId : string.Empty),
-            new Claim("dp", GetDominioPerms(userInfo, login)),
-            new Claim("ep", GetEstabelecimentoPerms(userInfo, login)),
-        };
+                new Claim("sub", GetSub(userInfo, login)),
+                new Claim("email", userInfo.Email ?? string.Empty),
+                new Claim("username", userInfo.NomeUsuario),
+                new Claim("uag", userInfo.IsSuperUsuario ? "true" : "false"),
+                new Claim("uad", login.EstabelecimentoId == null && userInfo.Dominios.FirstOrDefault(d => d.Id == login.DominioId)?.IsAdministrador == true ? "true" : "false"),
+                new Claim("d", login.DominioId != null && login.EstabelecimentoId == null ? login.DominioId : string.Empty),
+                new Claim("de", login.DominioId != null && login.EstabelecimentoId != null ? login.DominioId : string.Empty),
+                new Claim("e", login.EstabelecimentoId != null ? login.EstabelecimentoId : string.Empty),
+                new Claim("dp", GetDominioPerms(userInfo, login)),
+                new Claim("ep", GetEstabelecimentoPerms(userInfo, login)),
+            };
 
             var identity = new ClaimsIdentity(claims, "pwd");
             var user = new ClaimsPrincipal(identity);
