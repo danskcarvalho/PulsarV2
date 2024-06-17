cad758f74e5650f4b79aa83c268c10aeee6598bf
danskcarvalho <danskcarvalho@gmail.com>
2024-06-17 07:52:43 -0300

    More

--- Sources/Services/Identity/Identity.Domain/Aggregates/Usuarios/Usuario.Indexes.cs
+++ Sources/Services/Identity/Identity.Domain/Aggregates/Usuarios/Usuario.Indexes.cs
@@ -4,9 +4,9 @@ public partial class Usuario
 {
     public class Indexes : IndexDescriptions<Usuario>
     {
-        public static IX Email_v1 = Describe.Ascending(u => u.Email);
-        public static IX NomeUsuario_v1 = Describe.Ascending(u => u.NomeUsuario);
-        public static IX TermosBusca_Email_v1 = Describe.Text(u => u.TermosBusca).Ascending(u => u.Email);
+        public static IX Email_v1 = Describe.Ascending(u => u.Email).Unique();
+        public static IX NomeUsuario_v1 = Describe.Ascending(u => u.NomeUsuario).Unique();
+        public static IX TermosBusca_Email_v1 = Describe.Text(u => u.TermosBusca);
         public static IX DominiosBloqueados_Email_v1 = Describe.Ascending(u => u.DominiosBloqueados).Ascending(u => u.Email);
 
         public override string CollectionName => Constants.CollectionNames.USUARIOS;
