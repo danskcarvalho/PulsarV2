81bdde228357332adb946afbbf344fae122fcc68
danskcarvalho <danskcarvalho@gmail.com>
2024-06-17 07:55:59 -0300

    More

--- Sources/Services/Identity/Identity.Domain/Aggregates/Usuarios/Usuario.Indexes.cs
+++ Sources/Services/Identity/Identity.Domain/Aggregates/Usuarios/Usuario.Indexes.cs
@@ -6,7 +6,7 @@ public partial class Usuario
     {
         public static IX Email_v1 = Describe.Ascending(u => u.Email).Unique();
         public static IX NomeUsuario_v1 = Describe.Ascending(u => u.NomeUsuario).Unique();
-        public static IX TermosBusca_Email_v1 = Describe.Text(u => u.TermosBusca);
+        public static IX TermosBusca_Email_v1 = Describe.Text(u => u.TermosBusca).Ascending(u => u.Email);
         public static IX DominiosBloqueados_Email_v1 = Describe.Ascending(u => u.DominiosBloqueados).Ascending(u => u.Email);
 
         public override string CollectionName => Constants.CollectionNames.USUARIOS;
